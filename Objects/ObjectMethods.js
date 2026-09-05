a2da2c55870cc8636b7686b0976fb1e69ce113a2
mishra-pramit86 <mishra.pramit86@gmail.com>
2026-09-06 02:33:47 +0530

    added a code in object method

--- Objects/ObjectMethods.js
+++ Objects/ObjectMethods.js
@@ -18,6 +18,11 @@ const jonas = {
         return this.age;
     },
 
+    fullName: function()
+    {
+        return this.firstName + this.lastName
+    },
+
     getSummary: function()
     {
         return `${this.firstName} is a ${this.calcAge()} year old ${this.job} and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license `
@@ -25,3 +30,4 @@ const jonas = {
 };
 console.log(jonas.calcAge());
 console.log(jonas.getSummary());
+console.log(jonas.fullName());
